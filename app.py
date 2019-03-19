0db58f113d73b91718377f5b1afdc2a6b6d4b6b0
Paris Taylor <paristechprofile@gmail.com>
2019-03-19 11:43:21 -0700

    adds model for auth

--- app.py
+++ app.py
@@ -34,23 +34,3 @@ def barbers():
 if __name__ == '__main__':
   models.initialize()
   app.run(debug=DEBUG, port=PORT)
-
-# @app.route('/review', methods=['POST'])
-# @app.route('/review/<reviewid>', methods=['GET'])
-# def get_create_review(reviewid=None):
-#     from models import Review
-#     if reviewid == None:
-#         user = request.json['user']
-#         title = request.json['title']
-#         text = request.json['text']
-#         sub = request.json['sub']
-
-#         return Comments.create_comment(user, title, text, sub)
-#     else:
-#         return Comments.get_comment(commentid)
-
-# # @app.route('/comments')
-# # def hello_world():
-# #   return 'Hello World'
-
-
--- models.py
+++ models.py
@@ -1,15 +1,32 @@
 import datetime
 from peewee import *
 
+from flask_login import UserMixin
+from flask_bcrypt import generate_password_hash
+
 DATABASE = SqliteDatabase('fresh.db')
 
-class User(Model):
-  user_name = CharField()
-  email = CharField()
-  # pw = CharField()
-  
+class User(UserMixin, Model):
+  username = CharField(unique=True)
+  email = CharField(unique=True)
+  password  = CharField(max_length=100)
+  joined_at = DateTimeField(default=datetime.datetime.now)
+  is_admin = BooleanField(default=False)
+
   class Meta:
     database = DATABASE
+  
+  @classmethod
+  def create_user(cls, username, email, password, admin=false)
+    try:
+      cls.create(
+        username=username,
+        email=email,
+        password=generate_password_hash(password),
+        is_admin=admin
+      )
+    except IntegrityError:
+      raise ValueError("User already exists")
     
 class Barber(Model):
   name = CharField()
@@ -33,7 +50,7 @@ class Review(Model):
 
 def initialize():
   DATABASE.connect()
-  DATABASE.create_tables([Review], safe=True)
+  DATABASE.create_tables([User, Review], safe=True)
   DATABASE.close()
 
 
