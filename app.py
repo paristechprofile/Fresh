7ce042789df2ce87e3e8d4c8239ad4b4791e9822
Paris Taylor <paristechprofile@gmail.com>
2019-03-21 15:47:47 -0700

    minor changes

--- app.py
+++ app.py
@@ -133,17 +133,22 @@ def barbers(id=None):
 def delete_review(barberid, id):
   review_param = int(id)
   barber_param = int(barberid)
-  review = models.Review.get(models.Review.id == review_param)
-  review.delete_instance()
-  form = ReviewForm()
-  barber = models.Barber.get(models.Barber.id == barber_param)
-  reviews = barber.reviews
+  review = models.Review.get_or_none(review_param)
+  if str(review.user_id) == str(current_user.id):
+    review.delete_instance()
+    form = ReviewForm()
+    barber = models.Barber.get(models.Barber.id == barber_param)
+    reviews = barber.reviews
+    flash('you deleted your review')
+    return redirect(url_for('barbers', id=barber_param))
+  else:
+    flash('you cannot delete a review that is not yours')
   return redirect(url_for('barbers', id=barber_param))
 
 @app.route('/barbers/<barberid>/reviews/<id>/edit', methods=('GET', 'POST'))
 def edit_review(barberid, id):
-  barber_param = int(barberid)
   review_param = int(id)
+  barber_param = int(barberid)
   review = models.Review.get(models.Review.id == review_param)
   form = EditForm()
   barber = models.Barber.get(models.Barber.id == barber_param)
@@ -151,8 +156,12 @@ def edit_review(barberid, id):
     review.text = form.text.data
     review.rating = form.rating.data
     review.save()
-  return render_template("edit_form.html", id=barber_param, review=review, form=form)
-  
+    flash('you edited your review')
+    return redirect(url_for('barbers', id=barber_param))
+  else: 
+    flash('make sure to fill out both fields and that your review is 0-5')
+    
+    return render_template("edit_form.html", id=barber_param, review=review, form=form)
 if __name__ == '__main__':
   models.initialize()
   try:
